12edc115f44eac7c961370f34daa112540cdb144
David Meadows <davidtmeadows@gmail.com>
2024-07-05 14:55:07 -0400

    add more examples

--- examples/test.ts
+++ examples/test.ts
@@ -4,10 +4,34 @@ const phoebe = new Phoebe({
   apiKey: process.env['EBIRD_API_KEY'], // This is the default and can be omitted
 });
 
-async function main() {
-  const infoRetrieveResponse = await phoebe.ref.hotspot.info.retrieve('L99381');
+async function ref() {
+  const hotspot = await phoebe.ref.hotspot.info.retrieve('L99381');
 
-  console.log(infoRetrieveResponse.countryCode);
+  console.debug(hotspot);
+
+  const speciesGroups = await phoebe.ref.taxonomy.speciesGroups.list('ebird');
+
+  console.debug(speciesGroups);
+
+  const locales = await phoebe.ref.taxonomy.locales.list();
+
+  console.debug(locales);
+}
+
+async function observations() {
+  const phoebes = await phoebe.data.observations.recent.species.retrieve('US-NY', 'easpho');
+
+  console.debug(phoebes);
+
+  const recentChecklists = await phoebe.data.observations.recent.list('US-NY');
+
+  console.debug(recentChecklists);
+}
+
+async function main() {
+  await observations();
+
+  await ref();
 }
 
 main();
